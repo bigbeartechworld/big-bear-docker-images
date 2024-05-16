783ff5c3b9d478d9fc53e0c840fc267ee1d25ce3
Christopher <github.bigbeartechworld@emailcove.com>
2024-05-16 14:14:40 -0500

    chore: Update Dockerfile and entrypoint.sh for genmon

--- genmon/entrypoint.sh
+++ genmon/entrypoint.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+set -e
 
 # Ensure the /var/log/startup.log file exists
 touch /var/log/startup.log
+
+# Ensure the /git/genmon directory exists and has the necessary files
+if [ ! -f /git/genmon/startgenmon.sh ]; then
+  echo "Populating /git/genmon..."
+  git clone --depth 1 http://github.com/jgyates/genmon.git /git/genmon
+  chmod 775 /git/genmon/startgenmon.sh /git/genmon/genmonmaint.sh
+  rm -rf /git/genmon/.git
+fi
+
+# Ensure the /etc/genmon directory exists and has the necessary configuration files
+if [ ! -f /etc/genmon/genmon.conf ]; then
+  echo "Creating default configuration in /etc/genmon..."
+  mkdir -p /etc/genmon
+  cp /git/genmon/genmon.conf /etc/genmon/genmon.conf
+fi
 
 # Change the environment variable to use serial TCP in the genmon.conf file
 sed -i "s/use_serial_tcp = .*/use_serial_tcp = $USE_SERIAL_TCP/g" /etc/genmon/genmon.conf
