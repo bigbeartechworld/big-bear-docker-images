0c6e0858d9e3f1314510deed66868f1b5f3c80db
Christopher <github.bigbeartechworld@emailcove.com>
2024-08-28 19:54:58 -0500

    chore: Bump btop version to 0.0.7 and add default theme copying in entrypoint script for improved user experience

--- btop/entrypoint.sh
+++ btop/entrypoint.sh
@@ -4,6 +4,9 @@
 if [ ! -f /root/.config/btop/btop.conf ]; then
     # Copy default config if it doesn't exist
     cp /root/.config/btop.default/btop.conf /root/.config/btop/btop.conf
+
+    # Copy default themes if they don't exist
+    cp /usr/share/btop/themes /root/.config/btop/themes
 fi
 
 # Execute the provided command (in this case, gotty and btop)
