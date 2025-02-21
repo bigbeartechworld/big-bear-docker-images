3bfd58b6bbfa0b77fc1296315eb3b2e364bb6517
Christopher <github.bigbeartechworld@emailcove.com>
2025-02-21 11:05:18 -0600

    🔧 Move docker apps to Apps directory
    This is implemented for the README generation.

---
+++ Apps/pihole-unbound/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Set image name and tag
+IMAGE_NAME="bigbeartechworld/big-bear-pihole-unbound"
+TAG="2023.10.0"
+
+# Check if docker is installed
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Please install Docker first."
+    exit 1
+fi
+
+# Enable experimental CLI features
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+# Check if buildx is installed
+if ! docker buildx version &> /dev/null; then
+    echo "buildx is not installed or not available. Make sure you're using Docker 19.03 or later."
+    exit 1
+fi
+
+# Update root.hints
+curl -sSL https://www.internic.net/domain/named.root -o unbound/root.hints
+
+# Create or use a builder instance
+docker buildx create --use
+
+# Build the multi-arch image
+docker buildx build --progress plain --platform linux/amd64,linux/arm64,linux/arm/v7 -t $IMAGE_NAME:$TAG . --push > build.log
+
+# Feedback to the user
+if [ $? -eq 0 ]; then
+    echo "Successfully built and pushed multi-architecture image: $IMAGE_NAME:$TAG"
+else
+    echo "Failed to build the image. Please check the errors above."
+    exit 1
+fi
